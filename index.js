7acf974243715d2511135b7ac629a8a7b501421f
PK <nozumaz@gmail.com>
2023-04-24 10:01:54 -0400

    filter arrays

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter(drivers => drivers.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, startChars) {
+    return drivers.filter(drivers => drivers.substring(0,startChars.length) === startChars);
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(drivers => drivers.name === name);
+}
